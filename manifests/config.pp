b621be0868b47e59d49a28be1b4c30db8dcc4085
Chris Tessmer <chris.tessmer@onyxpoint.com>
2016-11-18 06:13:41 -0500

    (SIMP-1868) Remove `compliance_markup`
    This patch removes `compliance_markup` and provides general housekeeping
    
    SIMP-1962 #close
    SIMP-1868 #commment updated pupmod-simp-vsftpd
    
    Change-Id: I2fa17b816f9c32be1ff22d62c1a18b8e7132ab21

--- manifests/config.pp
+++ manifests/config.pp
@@ -206,7 +206,6 @@ class vsftpd::config (
   if $xferlog_file { validate_absolute_path($xferlog_file) }
   validate_absolute_path($ca_certs_file)
 
-  compliance_map()
 
   $_tcp_wrappers       = $::vsftpd::tcp_wrappers
   $_listen_port        = $::vsftpd::listen_port
